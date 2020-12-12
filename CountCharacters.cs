866cc09748762be997e87829da217e2fa4668e02
Eric O'Brien <36604687+cireneirbo@users.noreply.github.com>
2020-12-11 23:05:20 -0500

    Add project files.

---
+++ CountCharacters.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Studio_Counting_Characters
+{
+    class CountCharacters
+    {
+        public static string countCharactersInString(string text)
+        {
+            char[] textArray = text.ToCharArray();
+            List<string> textList = new List<string>();
+            foreach (char i in textArray)
+            {
+                textList.Add(textArray[i].ToString());
+            }
+            
+            Dictionary<string, int> stringTally = new Dictionary<string, int>();
+
+            foreach(string letter in textList)
+            {
+                if (stringTally.ContainsKey(letter))
+                {
+                    stringTally[letter] += 1;
+                }
+                else
+                {
+                    stringTally.Add(letter, 1);
+                }
+
+
+            }
+            string output = "";
+            foreach (KeyValuePair<string, int> letter in stringTally)
+            {
+                output += letter.Key + ": " + letter.Value + "\n";
+            }
+
+            //output = textArray.ToString();
+            //output = text.Length;
+            return output;
+        }
+    }
+}
+
+
+
+/*
+ * 
+ * int charCount = 0;
+                foreach(char letter in word)
+                {
+                    charCount += 1;
+                }*/
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Studio_Counting_Characters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. " +
+            "Nunc accumsan sem ut ligula scelerisque sollicitudin. Ut at sagittis augue. " +
+            "Praesent quis rhoncus justo. Aliquam erat volutpat. Donec sit amet suscipit metus, non lobortis massa. " +
+            "Vestibulum augue ex, dapibus ac suscipit vel, volutpat eget massa. Donec nec velit non ligula efficitur luctus.";
+
+
+            Console.WriteLine("Do you want to use your own text? y/n");
+            string inputBool = Console.ReadLine();
+            if (inputBool.ToLower() == "y" )
+            {
+                Console.WriteLine("Please input your text:");
+                string inputText = Console.ReadLine();
+                text = inputText;
+            }
+
+
+
+            Console.WriteLine(CountCharacters.countCharactersInString(text));
+
+        }
+    }
+}
